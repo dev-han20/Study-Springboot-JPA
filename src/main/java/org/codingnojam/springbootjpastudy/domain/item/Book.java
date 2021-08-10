383496746c3f0032fd2ecc97c85b2e5fe98302bf
CodingNoJam <hanwoodww@gmail.com>
2021-08-10 20:36:12 +0900

    upate

--- src/main/java/org/codingnojam/springbootjpastudy/domain/item/Book.java
+++ src/main/java/org/codingnojam/springbootjpastudy/domain/item/Book.java
@@ -3,12 +3,14 @@ package org.codingnojam.springbootjpastudy.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @Getter
 @Setter
-public class Book {
+@DiscriminatorValue("B")
+public class Book extends Item {
 
     private String itsn;
 
