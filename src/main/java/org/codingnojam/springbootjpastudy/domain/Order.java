cb374507ad5b491ecd54e9d6721a0aac548d6064
CodingNoJam <hanwoodww@gmail.com>
2021-08-12 04:29:34 +0900

    stduy

--- src/main/java/org/codingnojam/springbootjpastudy/domain/Order.java
+++ src/main/java/org/codingnojam/springbootjpastudy/domain/Order.java
@@ -19,21 +19,30 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne (fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne (fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
-    @Enumerated (EnumType.STRING)
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
