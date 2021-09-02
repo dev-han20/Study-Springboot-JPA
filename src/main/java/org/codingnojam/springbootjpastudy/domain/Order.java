d5f2ac6da11859dc816faec0a8d0b85dc273e94a
CodingNoJam <hanwoodww@gmail.com>
2021-09-02 23:05:20 +0900

    studing

--- src/main/java/org/codingnojam/springbootjpastudy/domain/Order.java
+++ src/main/java/org/codingnojam/springbootjpastudy/domain/Order.java
@@ -35,6 +35,11 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOders().add(this);
+    }
+
     public void addOrderItem(OrderItem orderItem) {
         this.orderItems.add(orderItem);
         orderItem.setOrder(this);
