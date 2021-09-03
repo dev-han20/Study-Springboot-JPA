67ec5079b89c3cab1a8070db288a41aaf76255ca
CodingNoJam <hanwoodww@gmail.com>
2021-09-03 22:42:05 +0900

    create ItemService class

---
+++ src/main/java/org/codingnojam/springbootjpastudy/service/ItemService.java
@@ -0,0 +1,30 @@
+package org.codingnojam.springbootjpastudy.service;
+
+import lombok.RequiredArgsConstructor;
+import org.codingnojam.springbootjpastudy.domain.item.Item;
+import org.codingnojam.springbootjpastudy.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long id) {
+        return itemRepository.findById(id);
+    }
+}
