c99f78c5a08597ea39677400ecda375be9753387
CodingNoJam <hanwoodww@gmail.com>
2021-09-03 20:26:34 +0900

    item code add

--- src/main/java/org/codingnojam/springbootjpastudy/domain/item/Item.java
+++ src/main/java/org/codingnojam/springbootjpastudy/domain/item/Item.java
@@ -3,6 +3,7 @@ package org.codingnojam.springbootjpastudy.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 import org.codingnojam.springbootjpastudy.domain.Category;
+import org.codingnojam.springbootjpastudy.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -26,5 +27,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    // 엔티티 내에 비즈니스 로직 넣기
+
+    // 재고 추가
+    public void addStockQuantity(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    // 재고 감소
+    public void removeStockQuantity(int stockQuantity) {
+        int currentStockQuantity = this.stockQuantity;
+        if (currentStockQuantity - stockQuantity < 0) {
+            throw new NotEnoughStockException("재고가 불 충분 함");
+        }
+        this.stockQuantity -= stockQuantity;
+    }
 
 }
